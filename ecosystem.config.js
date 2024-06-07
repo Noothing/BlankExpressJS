5aaa1d4f01ee598b85767f4e813a21fcdf6cb15a
Edgard Reykh <68498236+Noothing@users.noreply.github.com>
2024-06-07 14:08:19 +0400

    initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [
+    {
+      name: "Express backend blank",
+      script: 'npm run start',
+    }
+  ]
+};
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    // Get the referrer header
+    const referrer = req.get('Referer') || req.get('Referrer');
+    const referer2 = req.headers.referer || req.headers.referrer;
+    const origin = req.headers.origin;
+
+
+    // Log the referrer or send it in the response
+    res.send({
+        referrer,
+        referer2,
+        origin
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
