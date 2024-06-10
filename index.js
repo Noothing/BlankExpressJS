014340400ec0c227999ced7ce7ff8a549352b564
Edgard Reykh <68498236+Noothing@users.noreply.github.com>
2024-06-10 15:53:58 +0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const cors = require('cors')
+const path = require('path');
 const app = express()
 const port = 3001
 
 let i = 0
 
 app.get('/', (req, res) => {
+    // Log the referrer or send it in the response
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+api.get('/ping', (req, res) => {
     // Get the referrer header
     const referrer = req.get('Referer') || req.get('Referrer');
     const referer2 = req.headers.referer || req.headers.referrer;
@@ -13,18 +20,15 @@ app.get('/', (req, res) => {
     console.log(req)
     console.log(referrer, referer2, origin);
 
-    if (i === 0){
-        i = 1
-        fetch('http://dpg.noothing.xyz')
-    }
-
-    // Log the referrer or send it in the response
-    res.send({
-        referrer,
-        referer2,
-        origin
-    });
+    res.send(`Hi, you are from ${referrer}`)
 })
+
+app.use(cors({
+    origin: function (origin, callback) {
+        callback(null, true)
+    },
+    credentials: true,
+}))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
