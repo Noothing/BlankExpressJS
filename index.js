3b54b9e26f3b7fbf8ce212cf1cc3c3fdbe684031
Edgard Reykh <68498236+Noothing@users.noreply.github.com>
2024-06-10 15:27:06 +0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const subdomain = require('express-subdomain')
 
 app.get('/', (req, res) => {
     // Get the referrer header
