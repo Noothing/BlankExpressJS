49897509778bafbf1fa596fe980fc8aceca1309e
Edgard Reykh <68498236+Noothing@users.noreply.github.com>
2024-06-10 15:38:01 +0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 3001
-const subdomain = require('express-subdomain')
 
 app.get('/', (req, res) => {
     // Get the referrer header
@@ -20,24 +19,6 @@ app.get('/', (req, res) => {
     });
 })
 
-var router = express.Router();
-
-//api specific routes
-router.get('/', function (req, res) {
-    console.log('mem')
-
-    // Get the referrer header
-    const referrer = req.get('Referer') || req.get('Referrer');
-    res.send(`Welcome to our API on ${referrer}!`);
-});
-
-router.get('/users', function (req, res) {
-    res.json([
-        {name: "Brian"}
-    ]);
-});
-
-app.use(subdomain('api', router));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
